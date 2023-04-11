7c15b826cbe25baf04c21333ab912ebdb5be5492
woosin <ws1340cc@naver.com>
2023-04-11 14:49:54 +0900

    where절을 공부했습니다. 230411

--- 02_where절.sql
+++ 02_where절.sql
@@ -82,3 +82,104 @@ FROM
     employees
 WHERE
     hire_date LIKE '03%';
+
+SELECT
+    first_name,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date LIKE '%15';
+
+SELECT
+    first_name,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date LIKE '%05%';
+
+SELECT
+    first_name,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date LIKE '___05%';
+    
+-- IS NULL (null값을 찾음)
+SELECT
+    *
+FROM
+    employees
+WHERE
+    manager_id IS NULL;
+
+SELECT
+    *
+FROM
+    employees
+WHERE
+    commission_pct IS NULL;
+
+SELECT
+    *
+FROM
+    employees
+WHERE
+    commission_pct IS NOT NULL;
+    
+-- AND, OR
+-- AND가 OR보다 연산 순서가 빠름.
+SELECT
+    *
+FROM
+    employees
+WHERE
+    ( job_id = 'IT_PROG'
+      OR job_id = 'FI_MGR' )
+    AND salary >= 6000;
+    
+-- 데이터의 정렬 (SELECT 구문의 가장 마지막에 배치.)
+-- ASC: ascending 오름차순
+-- DESC: descending 내림차순
+
+SELECT
+    *
+FROM
+    employees
+ORDER BY
+    hire_date ASC;
+
+SELECT
+    *
+FROM
+    employees
+ORDER BY
+    hire_date DESC;
+
+SELECT
+    *
+FROM
+    employees
+WHERE
+    job_id = 'IT_PROG'
+ORDER BY
+    first_name ASC;
+
+SELECT
+    *
+FROM
+    employees
+WHERE
+    salary >= 5000
+ORDER BY
+    employee_id DESC;
+
+SELECT
+    first_name,
+    salary * 12 AS pay
+FROM
+    employees
+ORDER BY
+    pay ASC;
