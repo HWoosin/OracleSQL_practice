7098b4b477e1a3b2da69d2760a2272ed23f1cea1
woosin <ws1340cc@naver.com>
2023-04-20 11:14:21 +0900

    제약조건의 문제를 풀었습니다. 230420

--- 12-1_제약조건.sql
+++ 12-1_제약조건.sql
@@ -1,3 +1,4 @@
+
 -- 테이블 생성과 제약조건
 -- 테이블 열레벨 제약조건 (PRIMARY KEY, UNIQUE, NOT NULL, FOREIGN KEY, CHECK)
 -- PRIMARY KEY: 테이블의 고유 식별 컬럼입니다. (주요 키)
@@ -6,6 +7,9 @@
 -- FOREIGN KEY: 참조하는 테이블의 PRIMARY KEY를 저장하는 컬럼
 -- CHECK: 정의된 형식만 저장되도록 허용.
 
+DROP TABLE dept2;
+
+-- 컬럼 레벨 제약 조건 (컬럼 선언마다 제약조건 지정)
 CREATE TABLE dept2 (
     dept_no NUMBER(2) CONSTRAINT dept2_deptno_pk PRIMARY KEY,
     dept_name VARCHAR2(14) NOT NULL CONSTRAINT dept2_deptname_uk UNIQUE,
@@ -13,4 +17,98 @@ CREATE TABLE dept2 (
     dept_bonus NUMBER(10) CONSTRAINT dept2_bonus_ck CHECK(dept_bonus > 0),
     dept_gender VARCHAR2(1) CONSTRAINT dept2_gender_ck CHECK(dept_gender IN('M', 'F'))
 );
-desc dept2;
+
+-- 테이블 레벨 제약 조건(모든 열 선언 후 제약조건을 취하는 방식)
+CREATE TABLE dept2(
+    dept_no NUMBER(2),
+    dept_name VARCHAR2(14) NOT NULL,
+    loca NUMBER(4),
+    dept_bonus NUMBER(10),
+    dept_gender VARCHAR2(1),
+    
+    CONSTRAINT dept2_deptno_pk PRIMARY KEY(dept_no),
+    CONSTRAINT dept2_deptname_uk UNIQUE(dept_name),
+    CONSTRAINT dept2_loca_locid_fk FOREIGN KEY(loca) REFERENCES locations(location_id),
+    CONSTRAINT dept2_bonus_ck CHECK(dept_bonus > 0),
+    CONSTRAINT dept2_gender_ck CHECK(dept_gender IN('M', 'F'))
+);
+
+-- 외래 키(foreign key)가 부모테이블(참조테이블)에 없다면 INSERT가 불가능
+INSERT INTO dept2
+VALUES(10, 'gg', 4000, 100000, 'M'); -- 에러
+
+-- 주요 키(primary key)는 고유한 식별자여야 합니다.
+INSERT INTO dept2
+VALUES(20, 'hh', 1900, 100000, 'M');
+
+UPDATE dept2
+SET loca = 4000
+WHERE dept_no = 10; -- 실패(외래키 제약조건 위반)
+
+UPDATE dept2
+SET loca = 4000
+WHERE loca = 1800; -- 실패(외래키 제약조건 위반)
+
+-- 제약 조건 변경
+-- 제약 조건은 추가, 삭제가 가능합니다. 변경은 안됩니다.
+-- 변경하려면 삭제하고 새로운 내용으로 추가하셔야 합니다.
+
+DROP TABLE dept2;
+
+CREATE TABLE dept2(
+    dept_no NUMBER(2),
+    dept_name VARCHAR2(14) NOT NULL,
+    loca NUMBER(4),
+    dept_bonus NUMBER(10),
+    dept_gender VARCHAR2(1)
+);
+
+--pk 추가
+alter table dept2 add constraint dept_no_pk primary key(dept_no);
+--fk 추가
+alter table dept2 add constraint dept2_loca_locid_fk
+foreign key(loca) references locations(location_id);
+--check 추가
+alter table dept2 add CONSTRAINT dept2_gender_ck CHECK(dept_gender IN('M', 'F'));
+--unique 추가
+alter table dept2 add CONSTRAINT dept2_deptname_uk UNIQUE(dept_name);
+--not null은 열 수정형태로 변경.
+alter table dept2 modify dept_bonus number(10) not null;
+
+--제약 조건 삭제 (제약조건 이름으로)
+alter table dept2 drop constraint dept_no_pk;
+
+--제약조건 확인
+select * from user_constraints
+where table_name = 'DEPT2';
+
+
+
+/*문제 1*/
+drop table members;
+create table members(
+    M_NAME varchar2(3) not null,
+    M_NUM number(1) constraint mem_memnum_pk primary key,
+    REG_DATE date not null constraint mem_redate_uk unique,
+    GENDER varchar2(1) constraint mem_gender CHECK(GENDER IN('M', 'F')),
+    LOCA number(20) constraint mem_loca_loc_locid_fk references locations(location_id)
+);
+select * from members;
+
+insert into members values ('AAA', 1, '2018-07-01','M',1800);
+insert into members values ('BBB', 2, '2018-07-02','F',1900);
+insert into members values ('CCC', 3, '2018-07-03','M',2000);
+insert into members values ('DDD', 4, sysdate,'M',2000);
+
+/*문제 2*/
+select *from locations;
+
+select m.m_name, m.m_num, l.street_address, l.location_id
+from members m join locations l 
+on m.loca =l.location_id
+order by m_num asc;
+
+
+
+
+
