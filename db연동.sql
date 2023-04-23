6c7c5425282b5d554974a7ce3479d3554c3c2f80
user <user@DESKTOP-QRB3OPB>
2023-04-23 22:39:21 +0900

    집에서의 오라클디비 설치후 푸시 테스트 230423

--- db연동.sql
+++ db연동.sql
@@ -1,5 +1,7 @@
+--연동을 위한 sql문입니다. 230423
 drop table members;
-
+--create user hr identified by hr;
+--alter user hr account unlock identified by hr;
 create table members(
     mem_id varchar2(20) primary key,
     mem_pw varchar2(20) not null,
